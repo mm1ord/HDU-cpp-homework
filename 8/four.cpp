39f196a67c7ce4a21ec3a30eb6401a6c2fa0fce8
mm1ord <101250822+mm1ord@users.noreply.github.com>
2022-05-31 00:10:39 +0800

    Add files via upload

---
+++ 8/four.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+#include<list>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+bool isPrime(int n) {
+	if (n == 1 || n == -1 || n == 0)
+	{
+		return false;
+	}
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+
+int main() {
+	list<int> l;
+	int num1=0; int num2=0;
+	list<int>::iterator i;
+	cout << "请输入元素:" << endl;
+	copy(istream_iterator<int>(cin),istream_iterator<int>(),back_inserter(l));
+
+	for (i = l.begin(); i != l.end(); i++)
+	{
+		if (*i == 0)
+		{
+			num1++;
+		}
+		if (isPrime(*i))
+		{
+			num2++;
+		}
+	}
+
+	cout << endl;
+	cout << "0的个数：" << num1<<endl;
+	cout << "素数的个数" << num2 << endl;
+	return 0;
+
+} 
---
+++ 8/two.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<deque>
+
+using namespace std;
+
+int main()
+{
+
+	vector<int> v;
+	deque<int> d;
+
+	auto isPrime = [](int n) {
+		if (n == 1 || n == -1 || n == 0)
+		{
+			return 0;
+		}
+		for (int i = 2; i <= sqrt(n); i++)
+		{
+			if (n % i == 0)
+			{
+				return 0;
+			}
+		}
+		return 1;
+	};
+	//向vector中输入元素
+	copy(istream_iterator<int>(cin),istream_iterator<int>(),back_inserter(v));
+	cout << "vector：" << endl;
+	copy(v.begin(),v.end(),ostream_iterator<int>(cout," "));
+	cout << endl;
+	copy_if(v.begin(), v.end(), inserter(d,d.begin()), isPrime); 
+	cout << "deque：" << endl;
+	copy(d.begin(), d.end(), ostream_iterator <int>(cout, " "));
+
+}
